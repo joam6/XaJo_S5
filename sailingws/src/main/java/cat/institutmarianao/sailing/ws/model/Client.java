0c74e31e15318893e97542a4f14c189336fe90a3
joam6 <joseporiolam@gmail.com>
2024-12-16 19:55:50 +0100

    Validacio Client

--- sailingws/src/main/java/cat/institutmarianao/sailing/ws/model/Client.java
+++ sailingws/src/main/java/cat/institutmarianao/sailing/ws/model/Client.java
@@ -2,10 +2,20 @@ package cat.institutmarianao.sailing.ws.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import cat.institutmarianao.sailing.ws.validation.groups.OnUserCreate;
+import cat.institutmarianao.sailing.ws.validation.groups.OnUserUpdate;
 
 /* Lombok */
 @Data
@@ -20,10 +30,14 @@ public class Client extends User implements Serializable {
 
 	public static final int MIN_FULL_NAME = 3;
 	public static final int MAX_FULL_NAME = 100;
-
+	
+	@Size(min = MIN_FULL_NAME, max = MAX_FULL_NAME)
+	@NotNull
 	@Column(name = "full_name" , length = MAX_FULL_NAME)  // Mapea el atributo 'fullName' a la columna 'full_name' en la base de datos
 	protected String fullName;
 
+	@Pattern(regexp = "^[0-9]+$")
+	@Size(min = 10, max = 15)
     @Column(name = "phone")  // Mapea el atributo 'phone' a la columna 'phone' en la base de datos
 	protected Integer phone;
 	
